ce14661c1f603301fa582212d6e395e1126814e4
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-27 13:24:28 +0300

    update book-item

--- src/components/BookItem/BookItem.jsx
+++ src/components/BookItem/BookItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiOutlineStar, AiOutlineEye, AiOutlineCheck } from "react-icons/ai";
 
 import css from "./BookItem.module.css";
 
@@ -6,7 +7,7 @@ const BookItem = ({ book }) => {
   return (
     <li className={css.item}>
       <div className={css.imgWrapper}>
-        <img src={book.imgURL} alt="Book" width={220} height={320} />
+        <img src={book.imgURL} alt="Book" width={275} height={400} />
       </div>
       <div>
         <h2 className={css.title}>{book.title}</h2>
@@ -15,6 +16,22 @@ const BookItem = ({ book }) => {
         <p className={css.textContains}>
           В текcті є: <span className={css.contains}>{book.textContains}</span>
         </p>
+        <div className={css.info}>
+          <div className={css.likesWrapper}>
+            <AiOutlineStar />
+            <span>777</span>
+          </div>
+          <div className={css.viewsWrapper}>
+            <AiOutlineEye />
+            <span>333</span>
+          </div>
+          <div className={css.infoTextWrapper}>
+            <AiOutlineCheck />
+            <span>Повний текст</span>
+          </div>
+          <div className={css.decor}></div>
+          <div>{book.pages} стор.</div>
+        </div>
       </div>
     </li>
   );
