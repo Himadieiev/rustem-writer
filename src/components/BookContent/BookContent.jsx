8ce03d790f5e0041af03ee6e8a80330dd45c619c
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-28 21:50:39 +0300

    added content

--- src/components/BookContent/BookContent.jsx
+++ src/components/BookContent/BookContent.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 import css from "./BookContent.module.css";
 
-const BookContent = () => {
-  return <div className={css.bookContent}>BookContent</div>;
+const BookContent = ({ book }) => {
+  return (
+    <div className={css.bookContent}>
+      <h2 className={css.title}>Зміст книги: {book.content.length} гл.</h2>
+      <ul className={css.list}>
+        {book.content.map((item, index) => (
+          <li className={css.item} key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default BookContent;
