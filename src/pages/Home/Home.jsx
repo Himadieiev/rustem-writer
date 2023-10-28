39b75324011b7c4b184e109a93af367968c5f130
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-28 23:59:26 +0300

    update bg-imgs/home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { useSpring, animated } from "@react-spring/web";
 
 import css from "./Home.module.css";
 import Hero from "../../components/Hero/Hero";
 
 const Home = () => {
+  const props = useSpring({
+    from: { opacity: 0, transform: "translateY(-4rem)" },
+    to: { opacity: 1, transform: "translateY(0rem)" },
+    config: { duration: 1000 },
+  });
+
   return (
     <main className={css.home}>
       <div className="container">
         <Hero bg="bg-home" />
+        <animated.div style={props} className={css.greet}>
+          Вітаю Вас, мої друзі!
+        </animated.div>
       </div>
     </main>
   );
