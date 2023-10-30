a75510f845de435616f5b364c8492af5e029b99b
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-30 21:20:39 +0200

    added logout/user-info

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 import css from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
 import Button from "../Button/Button";
+import { useLogoutMutation } from "../../redux/slices/usersApiSlice";
+import { logout } from "../../redux/slices/authSlice";
 
 const Navbar = () => {
   const activeLink = `${css["link"]} ${css["activeLink"]}`;
   const normalLink = css.link;
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const handleLogout = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+      toast.success("Вихід пройшов успішно");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <header className={css.header}>
@@ -37,14 +60,26 @@ const Navbar = () => {
               Блог
             </NavLink>
           </nav>
-          <div className={css.btns}>
-            <NavLink to="/login">
-              <Button backgroundColor="login">Вхід</Button>
-            </NavLink>
-            <NavLink to="/register">
-              <Button backgroundColor="register">Реєстрація</Button>
-            </NavLink>
-          </div>
+          {!userInfo && (
+            <div className={css.btns}>
+              <NavLink to="/login">
+                <Button backgroundColor="login">Вхід</Button>
+              </NavLink>
+              <NavLink to="/register">
+                <Button backgroundColor="register">Реєстрація</Button>
+              </NavLink>
+            </div>
+          )}
+          {userInfo && (
+            <div className={css.userInfoWrapper}>
+              <p className={css.userName}>
+                Вітаю, <span className={css.name}>{userInfo.name}</span>
+              </p>
+              <div className={css.logout} onClick={handleLogout}>
+                <Button backgroundColor="login">Вихід</Button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className={css.decor}></div>
