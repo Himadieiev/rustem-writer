c30f71b9811128171d905539890205ae3b5cc3a2
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-30 18:38:03 +0200

    added login logic

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,38 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 import css from "./Login.module.css";
 import Button from "../../components/Button/Button";
+import { useLoginMutation } from "../../redux/slices/usersApiSlice";
+import { setCredentials } from "../../redux/slices/authSlice";
+import Loader from "../../components/Loader/Loader";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userInfo]);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
 
-  const handleLogin = () => {};
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await login({ email, password }).unwrap();
+      dispatch(setCredentials({ ...res }));
+      navigate("/");
+      toast.success("Ви успішно авторизовані");
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
 
   return (
     <main className={css.login}>
       <div className="container">
         <div className={css.formWrapper}>
           <h1 className={css.title}>Увійдіть до свого облікового запису</h1>
-          <form className={css.form}>
+          <form className={css.form} onSubmit={handleLogin}>
             <div>
               <label className={css.label}>
-                Ім'я користувача
+                Email
                 <br />
                 <input
                   className={css.input}
-                  type="text"
-                  value={username}
-                  onChange={handleUsernameChange}
-                  placeholder="Ім'я"
+                  type="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  placeholder="Email"
                 />
               </label>
             </div>
@@ -49,9 +78,18 @@ const Login = () => {
                 />
               </label>
             </div>
-            <div className={css.btnWrapper} onClick={handleLogin}>
-              <Button backgroundColor="login">Увійти</Button>
+            <div className={css.btnWrapper}>
+              <Button backgroundColor="login" type="submit">
+                Увійти
+              </Button>
             </div>
+            <div className={css.bottomText}>
+              Новий користувач?{" "}
+              <span className={css.link}>
+                <Link to={`/register`}>Зареєструйтесь</Link>
+              </span>
+            </div>
+            {isLoading && <Loader />}
           </form>
         </div>
       </div>
