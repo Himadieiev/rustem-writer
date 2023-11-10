8d7f10e7bf068f0a108f537afb04283b88b14901
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-11-10 22:58:52 +0200

    updated styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,21 +6,10 @@ import Hero from "../../components/Hero/Hero";
 import { home1, home2, home3 } from "./../../assets/images";
 
 const Home = () => {
-  const initialBg =
-    "linear-gradient(to bottom, rgba(226, 203, 119, 0.781), rgba(247, 247, 244, 0.5)";
-  const finalBg =
-    "linear-gradient(to bottom, rgba(247, 247, 244, 0.5), rgba(226, 203, 119, 0.781)";
-
   const greet = useSpring({
     from: { opacity: 0, transform: "translateY(-4rem)" },
     to: { opacity: 1, transform: "translateY(0rem)" },
     config: { duration: 1500 },
-  });
-
-  const bg = useSpring({
-    from: { backgroundImage: initialBg },
-    to: { backgroundImage: finalBg },
-    config: { duration: 1000 },
   });
 
   const elementRefs = [useRef(), useRef(), useRef()];
@@ -50,12 +39,7 @@ const Home = () => {
   });
 
   return (
-    <animated.main
-      style={{
-        backgroundImage: bg.backgroundImage,
-      }}
-      className={css.home}
-    >
+    <main className={css.home}>
       <div className="container">
         <Hero bg="bg-home" />
         <animated.div style={greet} className={css.greet}>
@@ -111,7 +95,7 @@ const Home = () => {
           </li>
         </ul>
       </div>
-    </animated.main>
+    </main>
   );
 };
 
