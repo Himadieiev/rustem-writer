ab305fb7bbb9d199e07a35ed0ce09b50e3d3a6b9
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-11-13 22:11:20 +0200

    added get posts

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaRegComment } from "react-icons/fa6";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import css from "./Blog.module.css";
 import Hero from "../../components/Hero/Hero";
 import Button from "../../components/Button/Button";
 import BlogModal from "../../components/BlogModal/BlogModal";
 import BlogForm from "../../components/BlogForm/BlogForm";
-
 import { selectUser } from "../../redux/auth/selectors";
+import { selectIsLoading, selectPosts } from "../../redux/posts/selectors";
+import { getPosts } from "../../redux/posts/thunks";
+import Loader from "../../components/Loader/Loader";
 
 const Blog = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const user = useSelector(selectUser);
+  const posts = useSelector(selectPosts);
+  const isLoading = useSelector(selectIsLoading);
+  const token = useSelector((state) => state.auth.token);
+
+  const dispatch = useDispatch();
+
+  const [isTokenLoaded, setIsTokenLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsTokenLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (isTokenLoaded && token) {
+      dispatch(getPosts());
+    }
+  }, [dispatch, isTokenLoaded, token]);
 
   const toggleModal = () => {
     setIsModalOpen((prevState) => !prevState);
@@ -28,57 +47,23 @@ const Blog = () => {
             <Button backgroundColor="register">Додати новий пост</Button>
           </div>
         )}
-        <div className={css.blogItem}>
-          <h2 className={css.title}>
-            Уривок глави 30. Епілог. Тиша та спокій.
-          </h2>
-          <p className={css.text}>
-            "Червоний серпанок затягнув простір навколо саркофагу, і Перший
-            Магістр зник у ньому, вийшовши з такого ж, тільки вже всередині
-            темниці. На дерев'яній лаві біля стіни сидів бранець, який повільно
-            підняв очі на гостя."
-          </p>
-          <div className={css.bottomItem}>
-            <div>23.07.2023</div>
-            <div className={css.commentWrapper}>
-              <FaRegComment />
-              <span>0</span>
-            </div>
-          </div>
-        </div>
-        <div className={css.blogItem}>
-          <h2 className={css.title}>Уривок глави 29. Ведмежа ущелина.</h2>
-          <p className={css.text}>
-            "Поспішаємо, друже, там ще є кого рятувати, - сказав акварієць. -
-            Тут ще двоє! - почулося звідкись зверху, і на скелях з'явилися
-            лучники Півночі. - Добийте їх! Вождь наказав не щадити нікого! -
-            відповіли голоси, і в Назара з Еверіном полетіли стріли."
-          </p>
-          <div className={css.bottomItem}>
-            <div>16.07.2023</div>
-            <div className={css.commentWrapper}>
-              <FaRegComment />
-              <span>0</span>
-            </div>
-          </div>
-        </div>
-        <div className={css.blogItem}>
-          <h2 className={css.title}>Уривок глави 28. Бранець.</h2>
-          <p className={css.text}>
-            "Таверна була невеликою, але столи розміщувалися досить щільно один
-            до одного, і від цього здавалося, що тут є не менше двох гарнізонів
-            солдатів. Командування дозволило воїнам, що вижили в Мірейському
-            лісі, відпочити з кухолем елю або пива, і загальним рішенням було
-            відзначити перемогу в місцевій таверні."
-          </p>
-          <div className={css.bottomItem}>
-            <div>01.07.2023</div>
-            <div className={css.commentWrapper}>
-              <FaRegComment />
-              <span>0</span>
-            </div>
-          </div>
-        </div>
+        {isLoading && <Loader />}
+        {posts.length !== 0 &&
+          posts
+            .map((item) => (
+              <div key={item._id} className={css.blogItem}>
+                <h2 className={css.title}>{item.title}</h2>
+                <p className={css.text}>"{item.text}"</p>
+                <div className={css.bottomItem}>
+                  <div>{item.date}</div>
+                  <div className={css.commentWrapper}>
+                    <FaRegComment />
+                    <span>0</span>
+                  </div>
+                </div>
+              </div>
+            ))
+            .reverse()}
       </div>
       {isModalOpen && (
         <BlogModal toggleModal={toggleModal}>
